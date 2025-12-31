0537cf0b0da2b222ab4a6f6a7bc59df28f48876a
Metzli <143217861+m3tzly@users.noreply.github.com>
2025-12-31 13:35:14 -0600

    Update pruebaCtrlG.py

--- Proyecto1_final/pruebaCtrlG.py
+++ Proyecto1_final/pruebaCtrlG.py
@@ -1,11 +1,15 @@
 # Ejemplo de uso
+import time
 from utils.ahk_ctrlG import CtrlGAHKManager
 
 ahk_manager = CtrlGAHKManager()
 ahk_manager.start_ahk()
 
-# Presionar Ctrl+G 5 veces
+time.sleep(2)  # Esperar a que AHK inicie
+
+# Presionar Ctrl+G 52 veces
 ahk_manager.presionar_ctrl_g(2)
+print("Se presionó Ctrl+G 2 veces")
 
 # Cuando termines
 ahk_manager.stop_ahk()
