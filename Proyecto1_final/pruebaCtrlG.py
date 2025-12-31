a616e817710ec36ba89f450d1778fee5c06adbb5
Metzli <143217861+m3tzly@users.noreply.github.com>
2025-12-31 15:46:26 -0600

    Cambio a crtl S

--- Proyecto1_final/pruebaCtrlG.py
+++ Proyecto1_final/pruebaCtrlG.py
@@ -1,14 +1,14 @@
 # Ejemplo de uso
 import time
-from utils.ahk_ctrlG import CtrlGAHKManager
+from Proyecto1_final.utils.ahk_ctrlS import CtrlSAHKManager
 
-ahk_manager = CtrlGAHKManager()
+ahk_manager = CtrlSAHKManager()
 ahk_manager.start_ahk()
 
 time.sleep(2)  # Esperar a que AHK inicie
 
 # Presionar Ctrl+G 52 veces
-ahk_manager.presionar_ctrl_g(2)
+ahk_manager.presionar_ctrl_s(2)
 print("Se presionó Ctrl+G 2 veces")
 
 # Cuando termines
