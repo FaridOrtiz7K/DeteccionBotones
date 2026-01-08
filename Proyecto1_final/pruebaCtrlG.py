59d9a0d485acec03b563f0f39fb4cdc96791b4d1
Metzli <143217861+m3tzly@users.noreply.github.com>
2026-01-08 13:50:17 -0600

    Revert "Update pruebaCtrlG.py"
    This reverts commit 0537cf0b0da2b222ab4a6f6a7bc59df28f48876a.

--- Proyecto1_final/pruebaCtrlG.py
+++ Proyecto1_final/pruebaCtrlG.py
@@ -1,15 +1,11 @@
 # Ejemplo de uso
-import time
 from utils.ahk_ctrlG import CtrlGAHKManager
 
 ahk_manager = CtrlGAHKManager()
 ahk_manager.start_ahk()
 
-time.sleep(2)  # Esperar a que AHK inicie
-
-# Presionar Ctrl+G 52 veces
+# Presionar Ctrl+G 5 veces
 ahk_manager.presionar_ctrl_g(2)
-print("Se presionó Ctrl+G 2 veces")
 
 # Cuando termines
 ahk_manager.stop_ahk()
